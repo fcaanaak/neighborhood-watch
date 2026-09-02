a00e0a90c50ab4399fa253e14886c729b30ab30d
fcaanaak <fcanak05@gmail.com>
2026-09-02 15:24:47 -0700

    Added support for getting channels in group

--- backend/django/nwchannels/models.py
+++ backend/django/nwchannels/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 from nwgroups.models import NWGroup
 
-# TODO:
-# 0. See if a division query is actually needed to get all the users in a group or if a join will suffice
-# 1. Rewrite the group division query to be a simple join if above applies
-# 2. Create a method to get all channels in a group
-# 3. Add that method to the groups viewset
-
 class NWChannel(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(max_length=256, blank=True)
     password = models.CharField(max_length=64, blank=True, null=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
+    owning_group = models.ForeignKey(NWGroup, on_delete=models.CASCADE, related_name='channels')
 
-    owning_group = models.ForeignKey(NWGroup, on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owning_group'],
+                name='nw_channel_',
+            )
+        ]
 
     def __str__(self):
         return f"{self.name} | {self.description}"
--- backend/django/nwusers/models/nwuser.py
+++ backend/django/nwusers/models/nwuser.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 from nwgroups.models import NWGroup
 
 class NWUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    auth_user = models.OneToOneField(User, on_delete=models.CASCADE)
     groups_joined = models.ManyToManyField(
         NWGroup,
         through= 'NWMembership',
-        related_name='nwgroups',
+        related_name='user_membership',
     )
 
     def __str__(self):
-        return self.user.username
+        return self.auth_user.username
 
 
 
